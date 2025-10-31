9b7ef845cdc563ff97401b2320fce7cfd1e55397
Lucas Didoné de Oliveira <144860505+didonelucas@users.noreply.github.com>
2025-10-31 08:28:21 -0300

    exercicio8.c

--- PrimeiroSemestre/Lista-Laços-de-repetição/exercicio8.c
+++ PrimeiroSemestre/Lista-Laços-de-repetição/exercicio8.c
@@ -1,13 +1,14 @@
 /*Faça um algoritmo que escreva os 30 primeiros termos da série 1,3,9,27...*/
 
 #include <stdio.h>
+#include <math.h>
 
 int main(){
-	int c, n=1;
-	printf("%d\n",n);
-		for(c=1; c<=29; c++){
-			n=n*3
-			printf("%d\n",n);
+		int c;
+		for(c=0; c<=29; c++){
+			printf("%.0f\n",pow(3,c));
 		}
 }
+
+
 
