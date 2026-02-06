16c4d92685cd168336d9cbe073528b06723e0b16
Lucas Didoné de Oliveira <144860505+didonelucas@users.noreply.github.com>
2026-02-06 15:26:45 -0300

    Update exercicio1.c

--- PrimeiroSemestre/ListaStructs/exercicio1.c
+++ PrimeiroSemestre/ListaStructs/exercicio1.c
@@ -4,27 +4,23 @@
 struct horario{
     int hora, min, seg;
 };
-struct horario h1, h2;
+struct horario h1[2];
 
 int main()
 {
-    int s1, s2, dif, h,m,s;
-    
+    int s1, s2, dif, h,m,s, i;
+
+    for (i=0; i<2;i++){
     printf("Digite a hora H1\n");
-    scanf("%d",&h1.hora);
+    scanf("%d",&h1[i].hora);
     printf("Digite o minuto H1\n");
-    scanf("%d",&h1.min);
+    scanf("%d",&h1[i].min);
     printf("Digite o segundo H1\n");
-    scanf("%d",&h1.seg);
-    printf("Digite a hora H2\n");
-    scanf("%d",&h2.hora);
-    printf("Digite o minuto H2\n");
-    scanf("%d",&h2.min);
-    printf("Digite o segundo H2\n");
-    scanf("%d",&h2.seg);
+    scanf("%d",&h1[i].seg);
+    }
     
-    s1= h1.hora * 3600 + h1.min*60 + h1.seg;
-    s2= h2.hora * 3600 + h2.min*60 + h2.seg;
+    s1= h1[0].hora * 3600 + h1[0].min*60 + h1[0].seg;
+    s2= h1[1].hora * 3600 + h1[1].min*60 + h1[1].seg;
     
     dif = abs(s1-s2);
     
