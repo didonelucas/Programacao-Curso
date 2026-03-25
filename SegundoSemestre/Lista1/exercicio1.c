58b89acf9a7743b76c9a87dfee58ef07a7bb9a71
Lucas Didoné de Oliveira <144860505+didonelucas@users.noreply.github.com>
2026-03-25 14:24:18 -0300

    Update exercicio1.c

--- SegundoSemestre/Lista1/exercicio1.c
+++ SegundoSemestre/Lista1/exercicio1.c
@@ -1,1 +1,15 @@
 /*Escreva um programa para ler o raio de um círculo, calcular e escrever a sua área.*/
+
+#include <stdio.h>
+#define PI 3.1416
+
+int main()
+{   
+    float raio, area;
+    printf("Digite o raio do círculo\n");
+    scanf("%f",&raio);
+    area=PI*raio*raio;
+    printf("A área do círculo é: %f",area);
+
+    return 0;
+}
