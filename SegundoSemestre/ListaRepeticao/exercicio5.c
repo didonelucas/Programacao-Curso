b147f3ea02ee879827f7dda23262e54384af736c
Lucas Didoné de Oliveira <144860505+didonelucas@users.noreply.github.com>
2026-04-01 14:42:21 -0300

    Add program to sum divisors and check for primality
    The program calculates the sum of divisors of an integer and checks if it is prime.

---
+++ SegundoSemestre/ListaRepeticao/exercicio5.c
@@ -0,0 +1,22 @@
+/*Faça um programa que recebe um número inteiro e imprime o somatório de seus
+divisores.*/
+#include <stdio.h>
+
+int main()
+{   
+    int n,i,soma=0;
+    printf("Digite um número");
+    scanf("%d",&n);
+    for(i=n; i>=1;i--){
+        if(n%i==0){
+            soma=soma+1;
+        }
+    }
+    if(soma<=2){
+        printf("%d é número primo\n",n);
+    }
+    else{
+        printf("%d não é número primo\n",n);
+    }
+    return 0;
+}
