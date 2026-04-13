368d8c1507fe6f5a9b7ebc006c0497893dd41e5a
Lucas Didoné de Oliveira <144860505+didonelucas@users.noreply.github.com>
2026-04-13 16:42:11 -0300

    Update exercicio4.c

--- SegundoSemestre/ListaMatrizes/exercicio4.c
+++ SegundoSemestre/ListaMatrizes/exercicio4.c
@@ -1,5 +1,5 @@
 /*Faça um programa que lê n números inteiros (cujo valor máximo não ultrapassa 1000).
-Após, o programa imprime em ordem crescente os números não duplicados do vetor.*/
+Após, o programa imprime em ordem crescente os números do vetor.*/
 
 #include <stdio.h>
 #define tam 10
