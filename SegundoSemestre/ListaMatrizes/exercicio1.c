90795dcee8c3a54606d1ff7cc42f1603a8c42f5b
Lucas Didoné de Oliveira <144860505+didonelucas@users.noreply.github.com>
2026-04-13 15:01:02 -0300

    Add program to identify smallest number in array
    Implement program to find the smallest number in an array.

--- SegundoSemestre/ListaMatrizes/exercicio1.c
+++ SegundoSemestre/ListaMatrizes/exercicio1.c
@@ -1,1 +1,19 @@
-a
+/*Faça um programa que lê n números decimais, armazenando-os em um vetor. Após, o
+programa identifica o menor número do vetor e imprime sua posição.*/
+#include <stdio.h>
+#define n 10
+
+int main()
+{   
+    int i, vetor[n], menor=100000, posMenor;
+    for(i=0;i<n;i++){
+        printf("Digite um valor\n");
+        scanf("%d",&vetor[i]);
+        if(vetor[i]<menor){
+            menor=vetor[i];
+            posMenor = i;
+        }
+    }
+    printf("O menor valor digitado foi %d na posição %d do vetor",menor,posMenor);
+    return 0;
+}
