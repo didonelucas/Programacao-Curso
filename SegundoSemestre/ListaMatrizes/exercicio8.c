8cf7b0723f143eea78a1445395588456eb85f24b
Lucas Didoné de Oliveira <144860505+didonelucas@users.noreply.github.com>
2026-04-22 18:38:05 -0300

    Implement distance-based filling in matriz
    Added logic to fill a 2D array with 'X' and '.' based on distance from center.

--- SegundoSemestre/ListaMatrizes/exercicio8.c
+++ SegundoSemestre/ListaMatrizes/exercicio8.c
@@ -9,20 +9,30 @@ xxxxx
 ..x..*/
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {   
-    int n=0,i,j;
+    int n=0,i,j,centro,distanciaI, distanciaJ;
     char vetor[50][50];
     
     while(n<4 || n>49 || n%2==0 ){
         printf("Digite um valor ímpar na faixa 3 < n < 50");
         scanf("%d",&n);
     }
+    centro=n/2;
+    
     for(i=0;i<n;i++){
         for(j=0;j<n;j++){
-            if()
-           vetor[i][j]=('.');
+            distanciaI=abs(i-centro);
+            distanciaJ=abs(j-centro);
+            
+            if((distanciaI+distanciaJ)<=centro){
+                vetor[i][j]=('X');
+            }
+            else{
+                vetor[i][j]=('.');
+            }
         }
     }
     for(i=0;i<n;i++){
