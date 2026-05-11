9dd8a5be74954bcc70878a670fbb07b3c299ecc0
Lucas Didoné de Oliveira <144860505+didonelucas@users.noreply.github.com>
2026-05-11 17:02:44 -0300

    Add recursive function to calculate factorial
    Implement a function to calculate factorial using recursion.

--- SegundoSemestre/ListaFunçoes/exercicio1.c
+++ SegundoSemestre/ListaFunçoes/exercicio1.c
@@ -1,6 +1,6 @@
 /*Faça um programa que calcule e imprima o fatorial de um número N. Este programa deve
 usar uma função, a qual calcula o fatorial do número lido. Não esqueça de testar as
-restrições cabíveis a N.*/
+restrições cabíveis a N. UTILIZANDO RECURSÃO*/
 
 #include <stdio.h>
 
