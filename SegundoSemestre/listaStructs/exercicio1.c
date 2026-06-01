6f2c44143ea99ff15798cfb328dc8302012da2e5
Lucas Didoné de Oliveira <144860505+didonelucas@users.noreply.github.com>
2026-06-01 16:23:30 -0300

    Add structure for Cartesian points and I/O functions
    Implement structure to store Cartesian coordinates and read/print points.

--- SegundoSemestre/listaStructs/exercicio1.c
+++ SegundoSemestre/listaStructs/exercicio1.c
@@ -1,1 +1,24 @@
-a
+/*Construa uma estrutura para armazenar pontos (coordenadas x e y) no plano cartesiano. Crie
+um algoritmo que permita ler, armazenar e imprimir uma quantidade TAM de pontos.*/
+
+#include <stdio.h>
+#define TAM 5
+
+typedef struct {
+    int x;
+    int y;
+} coord;
+
+int main()
+{
+    coord ponto[TAM];
+    int i;
+    for(i=0; i<TAM;i++){
+        scanf("%d %d",&ponto[i].x,&ponto[i].y);
+    }
+    for(i=0;i<TAM; i++){
+        printf("Ponto %d:\n(%d,%d)\n", i+1 ,ponto[i].x,ponto[i].y);
+    }
+  
+    return 0;
+}
