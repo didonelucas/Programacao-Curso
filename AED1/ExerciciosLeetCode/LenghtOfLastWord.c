db4d4998a02931654a67e2c32d77957cc34dc4fb
Lucas Didoné de Oliveira <144860505+didonelucas@users.noreply.github.com>
2026-08-20 20:15:35 -0300

    Add comment for lengthOfLastWord function
    Added a comment explaining the function's purpose.

--- AED1/ExerciciosLeetCode/LenghtOfLastWord.c
+++ AED1/ExerciciosLeetCode/LenghtOfLastWord.c
@@ -1,3 +1,6 @@
+/*Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+A word is a maximal substring consisting of non-space characters only.*/
 int lengthOfLastWord(char* s) {
     int soma=0, i, tam;
 
