c4dbab91b871f661b886423b93a805519b139186
Lucas Didoné de Oliveira <144860505+didonelucas@users.noreply.github.com>
2026-08-25 17:23:34 -0300

    Enhance TwoSum.c with comments for clarity
    Added comments to clarify memory allocation and loop logic.

--- AED1/ExerciciosLeetCode/TwoSum.c
+++ AED1/ExerciciosLeetCode/TwoSum.c
@@ -7,10 +7,10 @@ You can return the answer in any order.*/
 int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
     int i, j, *saida;
     *returnSize = 2;
-    saida = malloc(sizeof(int)*2);
+    saida = malloc(sizeof(int)*2); //Saída são dois inteiros
 
     for(i=0;i<numsSize;i++){
-        for(j=i+1;j<numsSize;j++){
+        for(j=i+1;j<numsSize;j++){ //j=i+1 para não testar o mesmo número
             if(nums[i] + nums[j] == target){
                 saida[0] = i;
                 saida[1] = j;
