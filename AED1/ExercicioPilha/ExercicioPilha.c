a7922454a0a084d21e62882fdbc274b06d2a2e84
Lucas Didoné de Oliveira <144860505+didonelucas@users.noreply.github.com>
2026-08-28 20:49:34 -0300

    Implement menu for stack operations on people
    Added a menu-driven interface for managing a stack of people, including options to insert, delete, clear, and list people.

--- AED1/ExercicioPilha/ExercicioPilha.c
+++ AED1/ExercicioPilha/ExercicioPilha.c
@@ -21,7 +21,43 @@ bool PUSH(Pilha *pilha, Pessoa *item);
 void POP(Pilha *pilha, Pessoa *pessoa);
 
 int main(){
+    int opcao=0,i;
+    Pessoa pessoa;
+    Pilha pilha;
 
+    Reset(&pilha);
+    while(opcao!=5){
+        printf("Menu:\n1.Inserir Pessoa;\n2.Deletar pessoa do topo\n3.Limpar a Pilha\n4.Listar Pessoas\n5.Sair\n");
+        scanf("%d",&opcao);
+        getchar();
+        switch(opcao){
+            case 1:
+                printf("Digite o nome:\n");
+                fgets(pessoa.nome,30,stdin);
+                printf("Digite a idade:\n");
+                scanf("%d",&pessoa.idade);
+                if(PUSH(&pilha,&pessoa)){
+                    printf("Pessoa inserida!\n");
+                }else{
+                    printf("Pilha cheia!\n");
+                }
+                break;
+            case 2:
+                POP(&pilha,&pessoa);
+                break;
+            case 3:
+                Reset(&pilha);
+                break;
+            case 4:
+                for(i=0;i<pilha.topo;i++){
+                    printf("%s\n%d\n",&pilha.pessoas[i].nome,pilha.pessoas[i].idade);
+                }
+            case 5:
+                printf("Saindo...");
+                exit(1);
+                break;
+        }
+    }
     return 0;
 }
 
