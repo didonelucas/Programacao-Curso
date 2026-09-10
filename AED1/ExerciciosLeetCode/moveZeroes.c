e88daf0bfab90064577d5d313d20c18107a11534
Lucas Didoné de Oliveira <144860505+didonelucas@users.noreply.github.com>
2026-09-10 09:46:21 -0300

    Add comment for moveZeroes function
    Add a comment explaining the purpose of the moveZeroes function.

--- AED1/ExerciciosLeetCode/moveZeroes.c
+++ AED1/ExerciciosLeetCode/moveZeroes.c
@@ -1,3 +1,4 @@
+/*Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.*/
 void moveZeroes(int* nums, int numsSize) {
     int i, k=0;
 
